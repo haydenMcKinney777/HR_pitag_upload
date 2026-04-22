69b215636d742e8d3ad7703e6695f6a7a9102759
hayden <hemc7@sbcglobal.net>
2026-04-22 12:58:42 -0500

    added function to create final workbook

--- split_data.py
+++ split_data.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from openpyxl import Workbook
 from collections import defaultdict
 
 SKIP_TAG_PREFIXES = {
@@ -48,40 +49,68 @@ PLANTS = {
     "Wise"
 }
 
-def extract_plant_name(tagname, plants):
+def write_output(map):
+    wb = Workbook() #create a new workbook
+    default_sheet = wb.active
+    wb.remove(default_sheet)    #get rid of the default sheet that comes with creating a new workbook
+
+    for plant, taglist in sorted(map.items()):
+        ws = wb.create_sheet(title = f"{plant}_Create")
+        ws.append(['Create Tag'])   #create header column
+        # new_sheet = wb[ws]  #index the workbook by the plant_Create sheet name to grab the sheet we're working with and store that sheet in 'new_sheet'
+
+        row = 1
+        for tag in taglist:
+            ws.cell(row=row, column=1, value=tag)
+            row += 1
+
+    wb.save("fleet_performance_tags_to_create.xlsx")
+
+#helper function to extract only the plant name from the tag. E.g. if given tag is Hays 1_Gas Turbine_XYZ this function will extract 'Hays'
+def extract_plant_name(tagname):
     if pd.isna(tagname):
         return None
 
+    #remove leading & trailing whitespace
     tagname = str(tagname).strip()
 
+    #skip over the tags in our global 'skip' set
     for skip_prefix in SKIP_TAG_PREFIXES:
         if tagname == skip_prefix or tagname.startswith(skip_prefix + " "):
             return None
 
     #sort plants so plants with longer names are first in the list. e.g. if our tag was Lake Road_Fuel 'startwith()' will pick up 'Lake Road' first before 'Lake '
-    for plant in sorted(plants, key=len, reverse=True):
+    #which is required because plants like lake hubbard and lake road exist thus only grabbing "lake" introduces ambiguity. sorting gets rid of this issue.
+    for plant in sorted(PLANTS, key=len, reverse=True):
         if (tagname == plant or tagname.startswith(plant + " ") or tagname.startswith(plant + "_")):
             return plant
-
     return tagname.split(" ", 1)[0]
 
 def main():
     print("Starting main program...\n")
 
+    #get cwd so that we can grab the excel file with our data
     cwd = os.getcwd()
     file_path = f"{cwd}\\pitag_list.xlsx"
 
+    #read the excel file into a dataframe and initialize a map 
     df = pd.read_excel(file_path)
     plant_tag_mappings = defaultdict(list)
 
     for pitag in df["Create these POCPI tags"]:
-        plant = extract_plant_name(pitag, PLANTS)
+        plant = extract_plant_name(pitag)
         if plant is None:
             continue
         plant_tag_mappings[plant].append(pitag)
 
-    print(plant_tag_mappings.keys())
-    print("Program complete.\n")
+    # print(plant_tag_mappings.keys())
+    # print("\n")
+    # print(plant_tag_mappings)
+
+    #send the plant to pitag mappings to a new excel file to be written
+    write_output(plant_tag_mappings)
+
+    print("\n=======Program complete.=======\n")
 
 if __name__ == "__main__":
     try:
